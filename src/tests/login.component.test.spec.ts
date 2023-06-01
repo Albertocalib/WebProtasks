7a2a623811914b0a339f11182b173287c56b33c5
Albertocalib <43534336+Albertocalib@users.noreply.github.com>
2023-06-01 21:35:56 +0200

    Añadidos más test

---
+++ src/tests/login.component.test.spec.ts
@@ -0,0 +1,82 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { LogInComponent } from '../app/logIn/logIn.component';
+import { LoginService } from '../app/services/logIn.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { FormsModule } from "@angular/forms";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatIconModule } from "@angular/material/icon";
+import {of, throwError} from "rxjs";
+import {MatInputModule} from "@angular/material/input";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {Router} from "@angular/router";
+
+describe('LogInComponent', () => {
+  let component: LogInComponent;
+  let fixture: ComponentFixture<LogInComponent>;
+  let mockLoginService: jasmine.SpyObj<LoginService>;
+  let mockSnackBar: jasmine.SpyObj<MatSnackBar>;
+  let mockRouter: jasmine.SpyObj<Router>;
+
+  beforeEach(async () => {
+    mockLoginService = jasmine.createSpyObj('LoginService', ['logIn']);
+    mockSnackBar = jasmine.createSpyObj('MatSnackBar', ['open']);
+    await TestBed.configureTestingModule({
+      declarations: [LogInComponent],
+      imports: [
+        FormsModule,
+        MatFormFieldModule,
+        MatDialogModule,
+        MatIconModule,
+        RouterTestingModule,
+        MatInputModule,
+        BrowserAnimationsModule
+      ],
+      providers: [
+        { provide: LoginService, useValue: mockLoginService },
+        { provide: MatSnackBar, useValue: mockSnackBar },
+      ],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LogInComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should log in successfully', () => {
+    const email = 'test@example.com';
+    const password = 'password';
+    const mockUser = { id: 1, username: 'testuser', name: 'test' };
+
+    mockLoginService.logIn.and.returnValue(of(mockUser));
+
+    component.logIn(null, email, password);
+
+    expect(mockLoginService.logIn).toHaveBeenCalledWith(email, password);
+    expect(mockSnackBar.open).not.toHaveBeenCalled();
+  });
+
+  it('should handle login error', () => {
+    const email = 'test@example.com';
+    const password = 'password';
+    const mockError = 'Invalid credentials';
+
+    mockLoginService.logIn.and.returnValue(throwError(mockError));
+
+    component.logIn(null, email, password);
+
+    expect(mockLoginService.logIn).toHaveBeenCalledWith(email, password);
+    expect(mockSnackBar.open).toHaveBeenCalledWith(
+      'Invalid user or password',
+      'Cerrar',
+      { duration: 2000, verticalPosition: 'top' }
+    );
+  });
+});
---
+++ src/tests/main.board.test.spec.ts
@@ -0,0 +1,151 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+import {MatMenuModule, MatMenuTrigger} from '@angular/material/menu';
+import { of } from 'rxjs';
+import { MainBoardComponent } from '../app/mainBoard/main.board.component';
+import { BoardService } from '../app/services/board.service';
+import { Board } from '../app/board.model';
+import { AppComponent } from '../app/app.component';
+import { AddElementDialogComponent } from '../app/AddElementDialog/add.element.dialog.component';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatIconModule} from "@angular/material/icon";
+import {MatCardModule} from "@angular/material/card";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import SpyObj = jasmine.SpyObj;
+
+describe('MainBoardComponent', () => {
+  let component: MainBoardComponent;
+  let fixture: ComponentFixture<MainBoardComponent>;
+  let mockBoardService: SpyObj<BoardService>;
+  let mockAppComponent: SpyObj<AppComponent>;
+  let dialogMock: SpyObj<MatDialog>
+  let mockBoards: Board[]
+
+
+
+  beforeEach(async () => {
+    mockBoardService = jasmine.createSpyObj('BoardService', ['getBoards', 'createBoard', 'delete', 'copy', 'updateBoard']);
+    mockAppComponent = jasmine.createSpyObj('AppComponent', ['openBoard']);
+    dialogMock = jasmine.createSpyObj('MatDialog', {
+      open: {
+        afterClosed: function () {
+          return {
+            subscribe: function (callback: ReturnType<any>) {
+              callback( {  title: 'Board', photo_name:'abcd.jpg',photo: 'photo3.jpg' })
+            }
+          }
+        }
+      }
+    });
+
+    await TestBed.configureTestingModule({
+      imports: [RouterTestingModule, MatDialogModule,MatFormFieldModule,
+        MatInputModule,MatIconModule,MatCardModule,MatMenuModule,BrowserAnimationsModule],
+      declarations: [MainBoardComponent],
+      providers: [
+        { provide: BoardService, useValue: mockBoardService },
+        { provide: AppComponent, useValue: mockAppComponent },
+        { provide: MatDialog, useValue: dialogMock }
+      ]
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    mockBoards = [
+      { id: 1, name: 'Board 1', photo: 'photo1', file_id: { name: 'file1', content: 'content1', type: 'img' } },
+      { id: 2, name: 'Board 2', photo: 'photo2', file_id: { name: 'file2', content: 'content2', type: 'img' } }
+    ];
+    mockBoardService.getBoards.and.returnValue(of(mockBoards));
+    fixture = TestBed.createComponent(MainBoardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should get boards and set filteredBoards', () => {
+    mockBoardService.getBoards.and.returnValue(of(mockBoards));
+
+    component.ngOnInit();
+
+    expect(mockBoardService.getBoards).toHaveBeenCalled();
+    expect(component.boards).toEqual(mockBoards);
+    expect(component.filteredBoards).toEqual(mockBoards);
+    expect(mockAppComponent.boards).toEqual(mockBoards);
+  });
+
+  it('should open board when no menu triggers are open', () => {
+    mockAppComponent.openBoard.and.stub();
+
+    component.openBoard(mockBoards[0]);
+
+    expect(mockAppComponent.openBoard).toHaveBeenCalledWith(mockBoards[0]);
+  });
+
+
+  it('should filter boards by name', () => {
+    component.boards = mockBoards;
+
+    component.onInputChange('Board 1');
+
+    expect(component.filteredBoards).toEqual([mockBoards[0]]);
+  });
+
+  it('should create a board', () => {
+    const newBoard: Board = { name: 'Board', photo: 'photo3.jpg', file_id: Object({ name: 'abcd.jpg', content: 'photo3.jpg', type: 'img' }) };
+    mockBoardService.createBoard.and.returnValue(of(newBoard));
+
+    component.createBoard();
+
+    expect(dialogMock.open).toHaveBeenCalledWith(AddElementDialogComponent, {
+      data: { title: '', description: '', type: 'board', editMode: false }
+    });
+    expect(mockBoardService.createBoard).toHaveBeenCalledWith(newBoard);
+    expect(component.boards).toEqual([mockBoards[0], mockBoards[1], newBoard]);
+  });
+
+  it('should delete a board', () => {
+    const boardToDelete = mockBoards[0];
+    mockBoardService.delete.and.returnValue(of(true));
+
+
+    component.deleteBoard(boardToDelete);
+
+    mockBoardService.delete(boardToDelete.id!!).subscribe(result => {
+      expect(result).toBeTrue();
+      expect(component.boards.length).toEqual(1);
+    });
+  });
+
+  it('should copy a board', () => {
+    const boardToCopy = mockBoards[0];
+    const copiedBoard: Board = { id: 3, name: 'Board 1 (Copy)', photo: 'photo1', file_id: { name: 'file1', content: 'content1', type: 'img' } };
+    mockBoardService.copy.and.returnValue(of(copiedBoard));
+
+    component.copyBoard(boardToCopy);
+
+    expect(mockBoardService.copy).toHaveBeenCalledWith(boardToCopy.id!!);
+    expect(component.boards.length).toEqual(3);
+  });
+
+  it('should create a board dialog config', () => {
+    const board = mockBoards[0];
+
+    const config = component.createBoardDialogConfig(board);
+
+    expect(config).toEqual({
+      title: board.name,
+      description: '',
+      type: 'board',
+      editMode: true,
+      photo: board.photo,
+      color: undefined,
+      photo_name: 'file1'
+    });
+  });
+
+});
