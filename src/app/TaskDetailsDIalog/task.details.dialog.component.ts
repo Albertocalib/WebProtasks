b8e786a4f5ce5b7da3387ccb91aad89a3a59cc0b
Albertocalib <43534336+Albertocalib@users.noreply.github.com>
2023-06-06 22:45:12 +0200

    Correcciones sonar

--- src/app/TaskDetailsDIalog/task.details.dialog.component.ts
+++ src/app/TaskDetailsDIalog/task.details.dialog.component.ts
@@ -61,13 +61,13 @@ export class TaskDetailsDialog implements OnInit{
     this.boardId=data.boardId
     this.selectedPriority= this.task.priority ? this.task.priority : Priority.NO_PRIORITY
     this.currentUser = JSON.parse(<string>localStorage.getItem('currentUser'));
-    this.tagService.getTagsInBoard(Number(this.boardId!!)).subscribe(response =>{
+    this.tagService.getTagsInBoard(+this.boardId).subscribe(response =>{
       if (response){
         this.boardTags=response
         this._filterTags(null)
       }
     })
-    this.loginService.getUsersInBoard(+this.boardId!!).subscribe(response =>{
+    this.loginService.getUsersInBoard(+this.boardId).subscribe(response =>{
       if (response){
         this.boardUsers=response
         this._filterAssigments(null)
